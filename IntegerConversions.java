2c97e0ef3d25623f7aa2da0422dfed3f282ce9e6
RDK7159357 <155869647+RDK7159357@users.noreply.github.com>
2025-04-03 15:33:02 +0530

    first commit

---
+++ IntegerConversions.java
@@ -0,0 +1,20 @@
+public class IntegerConversions{
+    public static void main(String[] args) {
+        String sage = args[0];
+
+        // 1 way
+        int age = Integer.parseInt(sage);
+        System.out.println(age);
+
+        // 2 way
+        @SuppressWarnings("removal")
+        Integer in = new Integer(sage);
+        age = in.intValue();
+        System.out.println(age);
+
+        // 3 way
+        in = Integer.valueOf(sage);
+        age = in.intValue();
+        System.out.println(age);
+    }
+}
