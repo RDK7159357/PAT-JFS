5e8208ee085ddd03eebd202149e070d453c26aaf
RDK7159357 <155869647+RDK7159357@users.noreply.github.com>
2025-04-10 14:33:57 +0530

    1.2

---
+++ pack1/A.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class A {
+    private int a;
+    int b;
+    protected int c;
+    public int d;
+
+    public void display(){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+    
+}
