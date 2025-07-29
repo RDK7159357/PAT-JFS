4d015aa756dde8e83085b6469c4976dba24b8250
RDK7159357 <155869647+RDK7159357@users.noreply.github.com>
2025-07-29 11:47:09 +0530

    1.71

--- abstract_class/Employee_Payroll.java
+++ abstract_class/Employee_Payroll.java
@@ -78,3 +78,14 @@ class EP_client{
 
     }
 }
+
+// If your file starts with:
+// package abstract_class;
+// then you must run using the fully qualified package name:
+// java -cp . abstract_class.EP_client Sales 105
+// but only from the parent directory of abstract_class/.
+
+// ie
+// javac abstract_class/EP_client.java
+// and then run it using:
+// java -cp . abstract_class.EP_client Sales 105
